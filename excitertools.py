b5455b4617708036ad13b09f31fe8b05777631aa
Caleb Hattingh <caleb.hattingh@gmail.com>
2020-02-17 23:12:15 +1000

    Added more functions from more_itertools

--- excitertools.py
+++ excitertools.py
@@ -1,8 +1,12 @@
 from __future__ import annotations
 import itertools
 import functools
-from typing import Iterable, Tuple, Any, TypeVar
+from typing import Iterable, Tuple, Any, TypeVar, List, Iterator, Sequence
 import collections.abc
+from types import MethodType
+from more_itertools.more import bucket as bucket_class
+
+import more_itertools
 
 __all__ = [
     'Iter',
@@ -26,8 +30,14 @@ class Iter:
     # def __getattr__(self, name):
     #     return globals()[name]
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator:
         return self.x
+
+    def __next__(self):
+        return next(self.x)
+
+    def collect(self) -> List:
+        return list(self)
 
     @classmethod
     def range(cls, *args) -> Iter:
@@ -42,7 +52,7 @@ class Iter:
     def enumerate(self) -> Iter:
         return Iter(enumerate(self.x))
 
-    def dict(self):
+    def dict(self) -> Iter:
         return Iter(dict(self.x).items)
 
     ###
@@ -50,28 +60,137 @@ class Iter:
     def map(self, *args) -> Iter:
         return Iter(map(*args, self.x))
 
-    def filter(self, *args):
+    def filter(self, *args) -> Iter:
         return Iter(filter(*args, self.x))
 
     def reduce(self, func, *args):
-        print(self.x)
         return functools.reduce(func, self.x, *args)
+
+    def sum(self):
+        return sum(self.x)
 
     ###
 
     @classmethod
-    def count(cls, *args):
+    def count(cls, *args) -> Iter:
         return cls(itertools.count(*args))
 
-    def cycle(self):
+    def cycle(self) -> Iter:
         return Iter(itertools.cycle(self.x))
 
     @classmethod
-    def repeat(cls, elem, n=None):
+    def repeat(cls, elem, n=None) -> Iter:
         return Iter(itertools.repeat(elem, n))
 
-    def collect(self):
-        return list(self)
+    def chain(self, *args):
+        return Iter(itertools.chain(self.x, *args))
 
-    def islice(self, *args):
+    def islice(self, *args) -> Iter:
         return Iter(itertools.islice(self.x, *args))
+
+
+
+    # more-itertools
+
+    def chunked(self, n: int) -> Iter:
+        return Iter(more_itertools.chunked(self.x, n))
+
+    def ichunked(self, n: int) -> List[Iter]:
+        return [
+            Iter(it) for it in more_itertools.ichunked(self.x, n)
+        ]
+
+    @classmethod
+    def sliced(cls, seq: Sequence, n: int) -> Iter:
+        return Iter(more_itertools.sliced(seq, n))
+
+    def distribute(self, n: int) -> Iter:
+        return Iter((Iter(x) for x in Iter(more_itertools.distribute(n, self.x))))
+
+    def divide(self, n: int) -> Iter:
+        return Iter(Iter(x) for x in more_itertools.divide(n, self.x))
+
+    def split_at(self, pred):
+        return Iter(more_itertools.split_at(self.x, pred))
+
+    def split_before(self, pred):
+        return Iter(more_itertools.split_before(self.x, pred))
+
+    def split_after(self, pred):
+        return Iter(more_itertools.split_after(self.x, pred))
+
+    def split_into(self, sizes):
+        return Iter(more_itertools.split_into(self.x, sizes))
+
+    def split_when(self, pred):
+        return Iter(more_itertools.split_when(self.x, pred))
+
+    def bucket(self, key, validator=None):
+
+        class _bucket(more_itertools.bucket):
+            def __iter__(self):
+                return Iter(super().__iter__())
+
+            def __getitem__(self, item):
+                return Iter(super().__getitem__(item))
+
+        return _bucket(self.x, key, validator=validator)
+
+    def unzip(self):
+        return Iter(Iter(x) for x in more_itertools.unzip(self.x))
+
+    def grouper(self, n: int, fillvalue=None) -> Iter:
+        return Iter(more_itertools.grouper(self.x, n, fillvalue=fillvalue))
+
+    def partition(self, pred) -> Iter:
+        left, right = more_itertools.partition(pred, self.x)
+        return Iter((Iter(left), Iter(right)))
+
+    # Lookahead and lookback
+
+    def spy(self, n=1) -> Tuple[Iter, Iter]:
+        head, iterable = more_itertools.spy(self.x, n)
+        return Iter(head), Iter(iterable)
+
+    def peekable(self) -> more_itertools.peekable:
+
+        class _peekable(more_itertools.peekable):
+            def __iter__(self):
+                return Iter(super().__iter__())
+
+            def __getitem__(self, item):
+                return super().__getitem__(item)
+
+            # TODO: need to somehow combine peekable and Iter
+            # def prepend(self, *items) -> Iter:
+            #     super().prepend(*items)
+            #     return Iter(self)
+
+        return _peekable(self.x)
+
+    def seekable(self) -> more_itertools.seekable:
+
+        class _seekable(more_itertools.seekable):
+            def __iter__(self):
+                return Iter(super().__iter__())
+
+        return _seekable(self.x)
+
+    # Windowing
+
+    '''
+    more_itertools.windowed
+    more_itertools.substrings
+    more_itertools.substrings_indexes
+    more_itertools.stagger
+    more_itertools.pairwise
+    '''
+
+    # Augmenting
+
+
+
+    # Other
+
+    def nth(self, n, default=None):
+        return next(self.islice(n, None), default)
---
+++ tests/test_more_itertools.py
@@ -0,0 +1,166 @@
+import pytest
+
+from excitertools import Iter
+
+
+def test_chunked():
+    it = Iter(range(12))
+    assert it.chunked(3).collect() == [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [9, 10, 11],
+    ]
+
+
+def test_ichunked():
+    it = Iter(range(12))
+    chunks = it.ichunked(3)
+    assert list(chunks[3]) == [9, 10, 11]
+    assert list(chunks[2]) == [6, 7, 8]
+    assert list(chunks[1]) == [3, 4, 5]
+    assert list(chunks[0]) == [0, 1, 2]
+
+
+def test_sliced():
+    seq = list(range(4))
+    assert Iter.sliced(seq, 2).collect() == [[0, 1], [2, 3]]
+    assert Iter.sliced(seq, 3).collect() == [[0, 1, 2], [3]]
+
+
+def test_distribute():
+    g1, g2 = Iter.range(6).distribute(2)
+    assert g1.collect() == [0, 2, 4]
+    assert g2.collect() == [1, 3, 5]
+
+
+def test_divide():
+    g1, g2 = Iter.range(6).divide(2)
+    assert g1.collect() == [0, 1, 2]
+    assert g2.collect() == [3, 4, 5]
+
+
+def test_split_at():
+    it = Iter('abcdcba').split_at(lambda x: x == 'b')
+    assert it.collect() == [
+        ['a'],
+        ['c', 'd', 'c'],
+        ['a'],
+    ]
+
+
+def test_split_before():
+    it = Iter('OneTwo').split_before(lambda s: s.isupper())
+    assert it.collect() == [
+        ['O', 'n', 'e'],
+        ['T', 'w', 'o'],
+    ]
+
+
+def test_split_after():
+    it = Iter('one1two2').split_after(lambda s: s.isdigit())
+    assert it.collect() == [
+        ['o', 'n', 'e', '1'],
+        ['t', 'w', 'o', '2'],
+    ]
+
+
+def test_bucket():
+    iterable = ['a1', 'b1', 'c1', 'a2', 'b2', 'c2', 'b3']
+    s = Iter(iterable).bucket(key=lambda x: x[0])
+    assert s['a'].collect() == ['a1', 'a2']
+    assert s['b'].collect() == ['b1', 'b2', 'b3']
+
+    iterable = ['a1', 'b1', 'c1', 'a2', 'b2', 'c2', 'b3']
+    s = Iter(iterable).bucket(key=lambda x: x[0])
+    a_iterable = s['a']
+    for x in a_iterable:
+        print(x)
+
+    iterable = ['a1', 'b1', 'c1', 'a2', 'b2', 'c2', 'b3']
+    s = Iter(iterable).bucket(key=lambda x: x[0])
+    a_iterable = s['a']
+    assert next(a_iterable) == 'a1'
+    assert next(a_iterable) == 'a2'
+
+
+def test_unzip():
+    iterable = [('a', 1), ('b', 2), ('c', 3), ('d', 4)]
+    it = Iter(iterable).unzip()
+    parts = it.collect()
+    assert parts[0].collect() == ['a', 'b', 'c', 'd']
+    assert parts[1].collect() == [1, 2, 3, 4]
+
+
+def test_grouper():
+    assert Iter('ABCDEFG').grouper(3, 'x').collect() == [
+        ('A', 'B', 'C'), ('D', 'E', 'F'), ('G', 'x', 'x')
+    ]
+
+
+def test_partition():
+    left, right = Iter(range(10)).partition(lambda x: x % 2 != 0)
+    assert left.collect() == [0, 2, 4, 6, 8]
+    assert right.collect() == [1, 3, 5, 7, 9]
+
+    def odd(x):
+        return x % 2 != 0
+
+    left = (
+        Iter(range(10))
+            .partition(odd)
+            .nth(0)
+            .collect()
+    )
+
+    right = (
+        Iter(range(10))
+            .partition(odd)
+            .nth(1)
+            .collect()
+    )
+    assert left == [0, 2, 4, 6, 8]
+    assert right == [1, 3, 5, 7, 9]
+
+
+def test_spy():
+    head, iterable = Iter('abcdefg').spy()
+    assert head.collect() == ['a']
+    assert iterable.collect() == ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+
+
+def test_peekable():
+    p = Iter(['a', 'b']).peekable()
+    assert p.peek() == 'a'
+    assert next(p) == 'a'
+
+    assert Iter([]).peekable().peek('hi') == 'hi'
+
+    p = Iter([1, 2, 3]).peekable()
+    p.prepend(10, 11, 12)
+    assert next(p) == 10
+    assert p.peek() == 11
+    # TODO: is there a clean way to keep p an Iter instance?
+    assert Iter(p).collect() == [11, 12, 1, 2, 3]
+
+    p = Iter([1, 2, 3]).peekable()
+    p.prepend(10, 11, 12)
+    assert p.peek() == 10
+    assert p[2] == 12
+    assert p[-1] == 3
+    assert next(p) == 10
+    assert p[2] == 1
+
+
+def test_seekable():
+    it = Iter.count().map(str).seekable()
+    assert (next(it), next(it), next(it)) == ('0', '1', '2')
+
+    it.seek(0)
+    assert (next(it), next(it), next(it)) == ('0', '1', '2')
+
+    assert next(it) == '3'
+
+    it.seek(10)
+    assert next(it) == '10'
+
