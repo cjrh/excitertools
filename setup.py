b32905e9ae15b645622af49e4586516adab884d8
Caleb Hattingh <caleb.hattingh@gmail.com>
2020-05-03 16:33:02 +1000

    Fix up setup.py

--- setup.py
+++ setup.py
@@ -93,14 +93,11 @@ setup(
         'Intended Audience :: Developers',
 
         # Pick your license as you wish
-        'License :: OSI Approved :: Apache License',
+        'License :: OSI Approved :: Apache Software License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
