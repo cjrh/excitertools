cef664ed4e7f238a5a2200282fe8f0656d117a17
Caleb Hattingh <caleb.hattingh@gmail.com>
2020-05-04 15:39:36 +1000

    Fix test that failed as a result of running black

--- tests/test_open.py
+++ tests/test_open.py
@@ -5,5 +5,5 @@ from excitertools import Iter
 def test_open():
     fname = pathlib.Path(__file__)
     x = Iter.open(fname).map(str.strip).filter(None).map(str.upper).collect()
-    assert len(x) == 14
+    assert len(x) == 7
     assert x[0] == "IMPORT PATHLIB"
