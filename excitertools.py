aa795258fab4fb8dcc67293f97f97d2b8f2b0b4e
Caleb Hattingh <caleb.hattingh@gmail.com>
2021-10-23 22:12:09 +1000

    Branch coverage

--- excitertools.py
+++ excitertools.py
@@ -3827,11 +3827,10 @@ class Iter(Generic[T]):
         """
         Reference: `more_itertools.consumer <https://more-itertools.readthedocs.io/en/stable/api.html?highlight=numeric_range#more_itertools.consumer>`_
 
-        Some ideas around a reverse iterator as a sink. The requirement to
-        first "next" a just-started generator before you can send values
-        into it is irritating, but not insurmountable. This method will
-        automatically detect the "just-started generator" situation, do the
-        ``next()``, and then send in the first value as necessary.
+        Some ideas around a reverse iterator as a sink. Usually you have
+        first to "send" a ``None`` into a generator if you want to send
+        more values into it (or call ``next()`` on it), but we handle 
+        that automatically.
 
         Simple case:
 
@@ -3842,6 +3841,22 @@ class Iter(Generic[T]):
             ...     while True:
             ...         output.append((yield))
             >>> Iter.range(3).send_also(collector()).collect()
+            [0, 1, 2]
+            >>> output
+            [0, 1, 2]
+
+        However, if the caller already started the generator, that 
+        works too:
+
+        .. code-block:: python
+
+            >>> output = []
+            >>> def collector():
+            ...     while True:
+            ...         output.append((yield))
+            >>> g = collector()
+            >>> next(g)  # This "starts" the generator
+            >>> Iter.range(3).send_also(g).collect()
             [0, 1, 2]
             >>> output
             [0, 1, 2]
